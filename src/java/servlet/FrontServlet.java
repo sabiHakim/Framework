39ea749e78943fe74edea8f66d87f811fbd62680
sabiHakim <srakotoalimanana@gmail.com>
2023-03-07 16:25:04 +0300

    sprint1

---
+++ src/java/servlet/FrontServlet.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+public class FrontServlet extends HttpServlet {
+
+ 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter out = response.getWriter();
+        out.println("Servlet : Front Servlet");
+        out.println("");
+        out.println("Context Path :"+request.getContextPath());
+        out.println("");
+        out.println("URL :"+request.getRequestURL());
+        out.println("");
+        out.println("Parametre :");
+        Enumeration<String> liste = request.getParameterNames();
+        while(liste.hasMoreElements()){
+            String element = liste.nextElement();
+            String[] elementValues = request.getParameterValues(element);
+            for(int i=0 ; i<elementValues.length ; i++){
+                out.println(element+" ["+(i+1)+"]  => "+elementValues[i]);
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+
+}
